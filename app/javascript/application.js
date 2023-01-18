d2cc95e2a9dafc80bc0f4f0f8efc387f983818c6
MarioCesarBais <mariocfbais@gmail.com>
2023-01-18 20:10:45 -0300

    Resolvido logout e iniciado aperfeiçoamento front-end: CSS botôes navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,19 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// import "@hotwired/turbo-rails" // original do Rails 7
+import "@hotwired/turbo-rails" // original do Rails 7
 
 
 require("@rails/ujs").start()
 require("turbolinks").start()
-// require("./channels")
+require("./channels")
 require('./agendadas/index')
 require('./agendadas/agendar')
 
 import 'bootstrap';
 
 import { loadDynamicBannerText } from './components/banner';
-// import { initUpdateNavbarOnScroll } from './components/navbar';
 import { inicioP, fimP } from './agendadas/index';
 import { horas } from './agendadas/agendar';
 
@@ -25,7 +24,6 @@ const data = document.getElementById('data');
 
 document.addEventListener('turbolinks:load', () => {
   loadDynamicBannerText();
-  // initUpdateNavbarOnScroll();
   if (inicio) inicio.addEventListener('change', () => { inicioP(inicio) });
   if (fim) fim.addEventListener('change', () => { fimP(fim) });
   if (data) data.addEventListener('change', () => { horas(data) });
